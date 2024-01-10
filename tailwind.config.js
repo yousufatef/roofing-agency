8296bbc69107b897cf10d42b02af91a49ac4ed41
youssef <youssefatef@gmail.com>
2024-01-11 01:01:45 +0200

    edit header bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        borderColor: "rgba(255,255,255,0.12)"
+      }
+    },
   },
   plugins: [],
 }
