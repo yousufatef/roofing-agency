79d823a8f0a253cfa1ecc3ba5d8aa7b3e39303e3
youssef <youssefatef@gmail.com>
2024-01-10 02:51:05 +0200

    create header and make it responsive for all devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import Projects from "./pages/Projects"
+import ContactUs from "./pages/ContactUs"
+import NavBar from "./components/NavBar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <NavBar/>
+      <BrowserRouter>
+          <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<ContactUs/>}/>
+          </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
