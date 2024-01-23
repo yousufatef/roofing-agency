65f1f5f48b1487d53dd466c6b480634e57bbba54
youssef <youssefatef@gmail.com>
2024-01-23 21:03:11 +0200

    update header and make it responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,116 +1,81 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { FC } from "react"
+import { useState } from "react"
+const NavBar = () => {
+   const [links] = useState([
+      { name: "Home", url: "/" },
+      { name: "About", url: "/about" },
+      { name: "Services", url: "/services" },
+      { name: "Projects", url: "/projects" },
+      { name: "Contact", url: "/contact" },
+   ])
+   const [isOpen, setIsOpen] = useState(false)
+   const toggleNavBar = () => {
+      setIsOpen(!isOpen)
+   }
+   return (
+      <div className="bg-[url('/src/assets/headerBg.png')] bg-contain bg-center p-[40px]">
+      <div className="  hidden md:flex justify-between text-center py-3 text-white opacity-90 ">
+         <p>+201115737646</p>
+         <p>6 October, Giza, Egypt</p>
+         <ul className="flex justify-center">
+            {["fa-facebook", "fa-instagram", "fa-twitter", "fa-linkedin"].map(
+               (socialIcon, index) => (
+                  <li key={index} className="mr-2">
+                     <a href="/">
+                        <i className={`fa-brands ${socialIcon}`}></i>
+                     </a>
+                  </li>
+               ),
+            )}
+         </ul>
+     </div>
 
+      <nav className="flex justify-between text-center py-4">
+         <a href="/">
+            <img src="/src/assets/house-logo1-80x34.png" alt="Logo-img" />
+         </a>
+         {!isOpen ? <ul id="list" className="hidden md:flex justify-center items-center gap-4 text-white">
+            {links.map((link) => (
+               <li key={link.name} className="font-medium  opacity-75 hover:opacity-100">
+                  <a href={link.url} className="">
+                     {link.name}
+                  </a>
+               </li>
+            ))}
+         </ul> : <ul id="list" className="md:hidden block bg-red-900 text-white absolute left-0 top-0 w-[100%] h-[270px] overflow-hidden">
+             <a href="/" className="absolute top-[30px] left-[15px] ">
+            <img src="/src/assets/house-logo1-80x34.png" alt="Logo-img" className="pt-[1.5rem]"/>
+             </a>
+            <div className="pt-[30px]">
+            {links.map((link) => (
+                  <li key={link.name} className="text-[20px] font-bold pt-3">
+                  <a href={link.url} className="opacity-75  pt-5 hover:opacity-100 transition-all duration-200">
+                     {link.name}
+                  </a>
+               </li>
+            ))}
+            </div>
+         </ul>}
+         <div className="text-white hidden md:block">
+            <i className="fa-solid fa-globe"></i>
+            <select className="outline-none bg-transparent">
+               <option value="English" className=" text-black">
+                  English
+               </option>
+               <option value="Arabic" className=" text-black">
+                  Arabic
+               </option>
+            </select>
+         </div>
+         <span
+            className="text-white text-[35px] absolute right-6 cursor-pointer md:hidden"
+            onClick={toggleNavBar}>
+            {isOpen ? <i className="fa-regular fa-circle-xmark"></i> : <i className="fa-solid fa-bars"></i>}
+         </span>
+      </nav>
+      <h1
+         className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{ location.pathname.replace("/","")}</h1>
+     </div>   
+   )
+}
 
-interface NavBar {}
-
-
-const NavBar:FC<NavBar> = () => {
-  const [links] = useState(["About", "Services", "Projects", "Contact"]);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const location = useLocation()
-  const currentUrl = location.pathname
-  console.log(location, "location")
-  console.log(currentUrl, "currentUrl")
-
-  const toggleNavBar = () => {
-    setIsOpen(!isOpen);
-    const list = document.getElementById("list")!;
-    list.style.display = "flex"
-  };
-  return (
-    <div className="bg-[url('../src/assets/headerBg.png')] bg-contain bg-center p-4">
-      {/* create above navBar that contain number and address and social-links*/}
-      <div
-        id="above-nav"
-        className="grid grid-cols-3 justify-around text-center py-3 text-white opacity-[0.9] max-md:hidden border-b-[1px] border-borderColor max-md:border-0"
-      >
-        <div>
-          <p>+201115737646</p>
-        </div>
-        <div>
-          <p>6 October, Giza, Egypt</p>
-        </div>
-        <ul className="flex justify-center">
-          <li className="mr-2">
-            <a href="/">
-              <i className="fa-brands fa-facebook"></i>
-            </a>
-          </li>
-          <li className="mr-2">
-            <a href="/">
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-          </li>
-          <li className="mr-2">
-            <a href="/">
-              <i className="fa-brands fa-twitter"></i>
-            </a>
-          </li>
-          <li className="mr-2">
-            <a href="/">
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      {/* create navBar that contain Logo and links and btn */}
-      <div
-        id="nav"
-        className="grid grid-cols-3 justify-around text-center py-4 "
-      
-      >
-        <div className="flex justify-center max-md:mb-6">
-          <a href="/">
-            <img src="../../src/assets/house-logo1-80x34.png" alt="Logo-img" />
-          </a>
-        </div>
-
-        <ul
-          id="list"
-          className="flex justify-center max-md:bg-[url('../src/assets/headerBg.png')] bg-contain text-white max-md:flex max-md:flex-col max-md:mb-5 max-md:absolute max-md:item-center max-md:bg-white max-md:w-full max-md:left-0 max-md:top-0 max-md:py-5 max-md:hidden"
-        >
-          <li className="max-md:mb-2 max-md:font-medium max-md:text-lg">
-            <a href="/" className="px-2">
-              Home
-            </a>
-          </li>
-          {links.map((link) => (
-            <li
-              key={link}
-              className="max-md:mb-2 max-md:font-medium max-md:text-lg"
-            >
-              <a href={`/${link.toLocaleLowerCase()}`} className="px-2">
-                {link}
-              </a>
-            </li>
-          ))}
-        </ul>
-
-        {/* malty Lang */}
-        <div className="text-white max-md:mt-3 max-md:hidden">
-          <i className="fa-solid fa-globe"></i>
-          <select className="outline-none bg-transparent ml-1">
-            <option value="English" className="bg-transparent text-black">
-              English
-            </option>
-            <option value="Arabic" className="bg-transparent text-black">
-              Arabic
-            </option>
-          </select>
-        </div>
-        <span
-          className="  max-md:text-white text-3xl absolute right-6 cursor-pointer md:hidden"
-          onClick={toggleNavBar}
-        >
-          {isOpen ? "" : <i className="fa-solid fa-bars"></i>}
-        </span>
-      </div>
-      {/* heading */}
-      <h1 id="header"  className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{currentUrl.replace("/", " ")}</h1>
-    </div>
-  );
-};
-export default NavBar;
+export default NavBar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,27 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        borderColor: "rgba(255,255,255,0.12)",
-        mainColor: "#cb4154",
-        globalColor: "#131d3b"
-      }
+      colors: {
+        primary: "#9F1916",
+        secondary: "#09483F",
+        lightgray: "#F7F8FA",
+        purb:"#AC7AB5"
+      },
+      fontFamily: {
+        somar: [ 'Somar Sans', 'sans-serif' ],
+      },
+      screens: {
+        // => @media (max-width: 1535px) { ... }
+        "max-3xl": { max: "1535px" },
+        "max-2xl": { max: "1279px" },
+        "max-xl": { max: "1150px" },
+        "max-lg": { max: "1023px" },
+        "max-md": { max: "767px" },
+        "max-sm": { max: "639px" },
+        "max-xs": {max : "450px"},
+        "max-xxs": {max : "340px"},
+      },
     },
-  },
-  plugins: [],
+    },
+    plugins: [ 'prettier-plugin-tailwindcss' ],
 }
