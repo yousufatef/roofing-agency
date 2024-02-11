182cbb5d2146dd23613d3a1faaed3aeff8ebeff9
youssef <youssefatef@gmail.com>
2024-02-11 08:46:56 +0200

    update navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,77 +1,199 @@
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { navLinks } from "../const"
+// import { navLinks } from "../const"
+// const NavBar = () => {
+//    const [links] = useState(navLinks)
+//    const [isOpen, setIsOpen] = useState(false)
+//    const toggleNavBar = () => {
+//       setIsOpen(!isOpen)
+//    }
+//    return (
+//       <div className="bg-[url('/assets/headerBg.png')] bg-contain bg-center p-[40px]">
+//       <div className="flex justify-between text-center py-3 text-white opacity-90 ">
+//          <p className="hidden md:block">+201115737646</p>
+//          <p className="hidden md:block">6 October, Giza, Egypt</p>
+//          <ul className="flex justify-center">
+//             {["fa-facebook", "fa-instagram", "fa-twitter", "fa-linkedin"].map(
+//                (socialIcon, index) => (
+//                   <li key={index} className="mr-2">
+//                      <a href="/">
+//                         <i className={`fa-brands ${socialIcon}`}></i>
+//                      </a>
+//                   </li>
+//                ),
+//             )}
+//          </ul>
+//      </div>
+
+
+
+//       <nav className="flex justify-between text-center py-4">
+//          <a href="/">
+//             <img src="/assets/house-logo1-80x34.png" alt="Logo-img" />
+//          </a>
+//          {!isOpen ? <ul id="list" className="hidden md:flex justify-center items-center gap-4 text-white">
+//             {links.map((link) => (
+//                <li key={link.name} className="font-medium  opacity-75 hover:opacity-100">
+//                   <NavLink to={link.url}>
+//                      {link.name}
+//                   </NavLink>
+//                </li>
+//             ))}
+//             </ul>
+//                :
+//             <ul id="list" className="md:hidden block bg-[#4F1A2F] text-white absolute left-0 top-0 w-[100%] h-[270px] overflow-hidden">
+//              <a href="/" className="absolute top-[30px] left-[15px] mt-[10px]">
+//             <img src="/assets/house-logo1-80x34.png" alt="Logo-img" className="pt-[1.5rem]"/>
+//              </a>
+//             <div className="pt-[30px]">
+//             {links.map((link) => (
+//                   <li key={link.name} className="text-[20px] font-bold pt-3">
+//                   <NavLink to={link.url} className="opacity-75  pt-5 hover:opacity-100 transition-all duration-200">
+//                      {link.name}
+//                   </NavLink>
+//                </li>
+//             ))}
+//             </div>
+//          </ul>}
+//          <div className="text-white block">
+//             <i className="fa-solid fa-globe"></i>
+//             <select className="outline-none bg-transparent">
+//                <option value="English" className=" text-black">
+//                   English
+//                </option>
+//                <option value="Arabic" className=" text-black">
+//                   Arabic
+//                </option>
+//             </select>
+//          </div>
+//          <span
+//             className="text-white text-[35px] absolute right-6 cursor-pointer md:hidden"
+//             onClick={toggleNavBar}>
+//             {isOpen ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i>}
+//          </span>
+//       </nav>
+//       <h1
+//          className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{location.pathname.replace("/","")}</h1>
+//      </div>
+//    )
+// }
+
+// export default NavBar
+
+
 const NavBar = () => {
    const [links] = useState(navLinks)
    const [isOpen, setIsOpen] = useState(false)
    const toggleNavBar = () => {
+      window.innerWidth > 100 ? setIsOpen(!isOpen) : ""
       setIsOpen(!isOpen)
    }
-   return (
-      <div className="bg-[url('/assets/headerBg.png')] bg-contain bg-center p-[40px]">
-      <div className="  hidden md:flex justify-between text-center py-3 text-white opacity-90 ">
-         <p>+201115737646</p>
-         <p>6 October, Giza, Egypt</p>
-         <ul className="flex justify-center">
+  return (
+     <>
+        {/* Large Screen*/}
+      <div className="max-md:hidden bg-[url('/assets/headerBg.png')] bg-contain bg-center p-[40px]">  
+         {/*Top NavBar*/}
+         <div className="flex justify-between text-center py-3 text-white opacity-90 ">
+            <p className="hidden md:block">+201115737646</p>
+            <p className="hidden md:block">6 October, Giza, Egypt</p>
+            <ul className="max-md:hidden flex justify-center">
             {["fa-facebook", "fa-instagram", "fa-twitter", "fa-linkedin"].map(
-               (socialIcon, index) => (
-                  <li key={index} className="mr-2">
-                     <a href="/">
-                        <i className={`fa-brands ${socialIcon}`}></i>
-                     </a>
-                  </li>
-               ),
-            )}
-         </ul>
-     </div>
-
-      <nav className="flex justify-between text-center py-4">
-         <a href="/">
-            <img src="/assets/house-logo1-80x34.png" alt="Logo-img" />
-         </a>
-         {!isOpen ? <ul id="list" className="hidden md:flex justify-center items-center gap-4 text-white">
-            {links.map((link) => (
+                  (socialIcon, index) => (
+                     <li key={index} className="mr-2">
+                        <a href="/">
+                           <i className={`fa-brands ${socialIcon}`}></i>
+                        </a>
+                     </li>
+                  ),
+               )}
+            </ul>
+        </div>
+        {/*NavBar*/}
+        <nav >
+           <div className="max-md:hidden flex justify-between items-center">
+               <a href="/">
+                  <img src="/assets/house-logo1-80x34.png" alt="Logo-img" />
+               </a>
+           <ul id="list" className="hidden md:flex justify-center items-center gap-4 text-white">
+             {links.map((link) => (
                <li key={link.name} className="font-medium  opacity-75 hover:opacity-100">
-                  <NavLink to={link.url} className="">
+                  <NavLink to={link.url}>
                      {link.name}
                   </NavLink>
                </li>
             ))}
-         </ul> : <ul id="list" className="md:hidden block bg-[#4F1A2F] text-white absolute left-0 top-0 w-[100%] h-[270px] overflow-hidden">
-             <a href="/" className="absolute top-[30px] left-[15px] mt-[10px]">
-            <img src="/assets/house-logo1-80x34.png" alt="Logo-img" className="pt-[1.5rem]"/>
-             </a>
-            <div className="pt-[30px]">
-            {links.map((link) => (
-                  <li key={link.name} className="text-[20px] font-bold pt-3">
-                  <NavLink to={link.url} className="opacity-75  pt-5 hover:opacity-100 transition-all duration-200">
-                     {link.name}
-                  </NavLink>
-               </li>
-            ))}
-            </div>
-         </ul>}
-         <div className="text-white hidden md:block">
+           </ul>
+           <div className="max-md:hidden text-white block">
             <i className="fa-solid fa-globe"></i>
             <select className="outline-none bg-transparent">
-               <option value="English" className=" text-black">
-                  English
-               </option>
-               <option value="Arabic" className=" text-black">
-                  Arabic
-               </option>
-            </select>
-         </div>
-         <span
-            className="text-white text-[35px] absolute right-6 cursor-pointer md:hidden"
-            onClick={toggleNavBar}>
-            {isOpen ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i>}
-         </span>
-      </nav>
-      <h1
-         className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{location.pathname.replace("/","")}</h1>
-     </div>   
-   )
+                <option value="English" className=" text-black">
+                   English
+                </option>
+                <option value="Arabic" className=" text-black">
+                   Arabic
+                </option>
+             </select>
+           </div>
+           </div>
+           <h1 className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{location.pathname.replace("/","")}</h1>
+        </nav>
+        </div>
+
+        {/*small Screen*/ }
+        {!isOpen ?
+         <div className="md:hidden bg-[url('/assets/headerBg.png')] bg-contain bg-center p-[40px]"> 
+           <div className="flex justify-between items-center">
+               <a href="/">
+                  <img src="/assets/house-logo1-80x34.png" alt="Logo-img" />
+              </a>
+              <span
+               className="text-white text-[35px] absolute right-6 cursor-pointer md:hidden"
+               onClick={toggleNavBar}>
+               {isOpen ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i>}
+             </span>
+           </div>
+         <h1 className="font-extrabold text-5xl bg-contain text-white p-7 text-center alien-center min-h-24">{location.pathname.replace("/","")}</h1>
+           </div> :          
+           <div className=" bg-[#4F1A2F] md:hidden bg-contain bg-center p-[40px]">
+           <div className="flex justify-between items-center">
+               <a href="/">
+                  <img src="/assets/house-logo1-80x34.png" alt="Logo-img" />
+              </a>
+              <span
+               className="text-white text-[35px] absolute right-6 cursor-pointer md:hidden"
+               onClick={toggleNavBar}>
+               {isOpen ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i>}
+             </span>
+           </div>
+             <ul id="list" className=" text-white  w-[100%] h-[270px] overflow-hidden">
+                           <div className="pt-[30px]">
+                          {links.map((link) => (
+                                 <li key={link.name} className="text-2xl font-bold pt-3">
+                                 <NavLink onClick={toggleNavBar} to={link.url} className="opacity-75  pt-5 hover:opacity-100 transition-all duration-200">
+                                    {link.name}
+                                 </NavLink>
+                             </li>
+                           ))}
+                  </div>
+              </ul>
+             <ul className="flex text-xl text-white mt-2">
+             {["fa-facebook", "fa-instagram", "fa-twitter", "fa-linkedin"].map(
+                (socialIcon, index) => (
+                   <li key={index} className="mr-2">
+                      <a href="/">
+                         <i className={`fa-brands ${socialIcon}`}></i>
+                      </a>
+                   </li>
+                ),
+             )}
+              </ul>
+           </div>
+        }
+
+     </>
+       
+  )
 }
 
 export default NavBar
